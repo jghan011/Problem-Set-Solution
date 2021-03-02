286c2e99e6ec546b5ac2a96c3ebad159cdd97bc6
Jason <jghan011@gmail.com>
2021-03-02 14:00:25 -0600

    added css,html. & JS

---
+++ index.js
@@ -0,0 +1,42 @@
+
+//     // PROBLEM 0 //
+
+// function reverseCharactersButNotWords(inputSentence) {
+//     var splitString = inputSentence.split('');
+
+//     var reverseString = splitString.reverse();
+
+//     var joinArray = reverseString.join('')
+   
+//     console.log(joinArray);
+//     return joinArray
+
+// }
+// reverseCharactersButNotWords("why are you here?");
+
+//     // PROBLEM 1//
+//         var total = 0
+
+//     for( var x = 0; x >= total; x++){
+//         total =+ x;
+//         x++;
+//      if(total % 2 === 0){ 
+//             console.log("foo");
+//         }
+
+//      if(total % 3 === 0 ){
+//           console.log("bar");
+//         }
+
+//      if(total % 6 === 0){
+//        console.log("baz");
+//         }
+
+//     else{
+//             console.log(x);
+//          }
+
+//     }
+
+    
+  
