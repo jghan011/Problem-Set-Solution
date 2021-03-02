f36f5c05e6904b8b6902b3604d38652e73b6d60c
Jason <jghan011@gmail.com>
2021-03-02 14:03:25 -0600

    new of js,css, & html

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 
-//     // PROBLEM 0 //
+    // PROBLEM 0 //
 
-// function reverseCharactersButNotWords(inputSentence) {
-//     var splitString = inputSentence.split('');
+function reverseCharactersButNotWords(inputSentence) {
+    var splitString = inputSentence.split('');
 
-//     var reverseString = splitString.reverse();
+    var reverseString = splitString.reverse();
 
-//     var joinArray = reverseString.join('')
+    var joinArray = reverseString.join('')
    
-//     console.log(joinArray);
-//     return joinArray
+    console.log(joinArray);
+    return joinArray
 
-// }
-// reverseCharactersButNotWords("why are you here?");
+}
+reverseCharactersButNotWords("why are you here?");
 
-//     // PROBLEM 1//
-//         var total = 0
+    // PROBLEM 1//
+        var total = 0
 
-//     for( var x = 0; x >= total; x++){
-//         total =+ x;
-//         x++;
-//      if(total % 2 === 0){ 
-//             console.log("foo");
-//         }
+    for( var x = 0; x >= total; x++){
+        total =+ x;
+        x++;
+     if(total % 2 === 0){ 
+            console.log("foo");
+        }
 
-//      if(total % 3 === 0 ){
-//           console.log("bar");
-//         }
+     if(total % 3 === 0 ){
+          console.log("bar");
+        }
 
-//      if(total % 6 === 0){
-//        console.log("baz");
-//         }
+     if(total % 6 === 0){
+       console.log("baz");
+        }
 
-//     else{
-//             console.log(x);
-//          }
+    else{
+            console.log(x);
+         }
 
-//     }
+    }
 
     
   
